d6adfdad516fc04e09860923c13a8e321a9295f4
fengshuo <326039018@qq.com>
2019-08-19 14:26:30 +0800

    change

--- pizza-app/src/components/about/History.vue
+++
@@ -1,9 +0,0 @@
-<template>
-        <div class="card text-dark bg-light mb-3">
-            <div class="card-header">历史订单</div>
-            <div class="card-body">
-                <h3 class="card-title">历史订单</h3>
-                <p class="card-text">88888888</p>
-            </div>
-        </div>
-    </template>
